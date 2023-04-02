ae598908d26ea7e8abb0c5712a54ff7ba7cbf29e
Bruno Jesus <bruno.fl.jesus@gmail.com>
2023-04-03 00:59:16 +0100

    Update about me

--- site/pages/about.php
+++ site/pages/about.php
@@ -1,22 +1,20 @@
 <p>My name is <span class="text-bold"> Bruno Jesus</span>,
-    I'm a software developer currently working for <a href="https://frontastic.cloud" class="text-marker-pink">Frontastic</a>.</p>
-<p>I hope some of my <a href="<?= BASE_URL; ?>index.php/blog" class="text-marker-orange">notes</a> are useful to you, if
-    you have
-    some suggestions or want to work with me please feel free to <a href="mailto:bruno.fl.jesus@gmail.com"
+    and I am a software developer currently working for <a href="https://commercetools.com" class="text-marker-pink">Commercetools</a>.</p>
+<p>I hope some of my <a href="<?= BASE_URL; ?>index.php/blog" class="text-marker-orange">notes</a> have been useful to you. If
+    you have any suggestions or would like to collaborate, please feel free to <a href="mailto:bruno.fl.jesus@gmail.com"
                                                                     class="text-marker-blue">contact me</a>.</p>
-
 
 <!-- WORK EXPERIENCE -->
 <h3 class="text-center">Work Experience</h3>
 
 <div class="timeline">
     <div class="item">
-        <div class="work-place">Frontastic GmbH</div>
+        <div class="work-place">Commercetools</div>
         <div class="job-meta">
             <div class="title">Software Developer</div>
             <div class="time">2022-02 / Present</div>
         </div>
-	<div class="job-desc">Development of <a href="https://frontastic.cloud" class="text-marker-pink">Frontastic</a> eCommerce solution</div>
+	<div class="job-desc">Development of <a href="https://commercetools.com/features/frontend" class="text-marker-pink">Commercetools Frontend</a></div>
     </div>
 
     <div class="item">
@@ -25,7 +23,7 @@
             <div class="title">Invited Assistant</div>
             <div class="time">2021-10 / Present</div>
         </div>
-        <div class="job-desc">Teaching Databases</div>
+        <div class="job-desc">Teaching Databases, Systems Integration and Software Engineering</div>
     </div>
     <div class="item">
         <div class="work-place">Mercedes-Benz.io @ Cloudmobility</div>
@@ -111,7 +109,7 @@
                   fill="black"/>
         </svg>
         <div class="language">Java</div>
-        <p>I use Java everyday at work since 2017 and it's being great.</p>
+        <p>It has been a part of my day-to-day work since 2017, and it's the language I have the most experience with.</p>
     </div>
     <div class="skill text-center">
         <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" aria-hidden="true"
@@ -122,7 +120,7 @@
                   fill="black"/>
         </svg>
         <div class="language">TypeScript</div>
-        <p>Started learning TypeScript and Angular in my spare time, now it's part of my job.</p>
+        <p>Learned it in order to develop in Angular in my spare time, and now it's become an integral part of my job.</p>
     </div>
     <div class="skill text-center">
         <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" aria-hidden="true"
@@ -133,13 +131,76 @@
                   fill="black"/>
         </svg>
         <div class="language">PHP</div>
-        <p>First language that I learned for web development. Used for this site.</p>
+        <p>The first language I learned for web development. Using it every day at Commercetools.</p>
     </div>
-</div>
-<div class="frameworks">
-    <div class="title">Frameworks</div>
-    <div class="framework">Spring (MVC, Cloud, Data, Security, Batch)</div>
-    <div class="framework">Hibernate</div>
-    <div class="framework">Angular 2+</div>
-    <div class="framework">CodeIgniter</div>
+    <div class="skill text-center">
+	<svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+	 viewBox="0 0 205.4 76.7" style="enable-background:new 0 0 205.4 76.7;" xml:space="preserve" width="160px" height="160px">
+<g>
+	<g>
+		<g>
+			<g>
+				<path d="M15.5,23.2c-0.4,0-0.5-0.2-0.3-0.5l2.1-2.7c0.2-0.3,0.7-0.5,1.1-0.5h35.7c0.4,0,0.5,0.3,0.3,0.6l-1.7,2.6
+					c-0.2,0.3-0.7,0.6-1,0.6L15.5,23.2z"/>
+			</g>
+		</g>
+	</g>
+	<g>
+		<g>
+			<g>
+				<path d="M0.4,32.4c-0.4,0-0.5-0.2-0.3-0.5l2.1-2.7c0.2-0.3,0.7-0.5,1.1-0.5h45.6c0.4,0,0.6,0.3,0.5,0.6l-0.8,2.4
+					c-0.1,0.4-0.5,0.6-0.9,0.6L0.4,32.4z"/>
+			</g>
+		</g>
+	</g>
+	<g>
+		<g>
+			<g>
+				<path d="M24.6,41.6c-0.4,0-0.5-0.3-0.3-0.6l1.4-2.5c0.2-0.3,0.6-0.6,1-0.6h20c0.4,0,0.6,0.3,0.6,0.7L47.1,41
+					c0,0.4-0.4,0.7-0.7,0.7L24.6,41.6z"/>
+			</g>
+		</g>
+	</g>
+	<g>
+		<g id="CXHf1q_5_">
+			<g>
+				<g>
+					<path d="M128.4,21.4c-6.3,1.6-10.6,2.8-16.8,4.4c-1.5,0.4-1.6,0.5-2.9-1c-1.5-1.7-2.6-2.8-4.7-3.8c-6.3-3.1-12.4-2.2-18.1,1.5
+						c-6.8,4.4-10.3,10.9-10.2,19c0.1,8,5.6,14.6,13.5,15.7c6.8,0.9,12.5-1.5,17-6.6c0.9-1.1,1.7-2.3,2.7-3.7c-3.6,0-8.1,0-19.3,0
+						c-2.1,0-2.6-1.3-1.9-3c1.3-3.1,3.7-8.3,5.1-10.9c0.3-0.6,1-1.6,2.5-1.6c5.1,0,23.9,0,36.4,0c-0.2,2.7-0.2,5.4-0.6,8.1
+						c-1.1,7.2-3.8,13.8-8.2,19.6c-7.2,9.5-16.6,15.4-28.5,17c-9.8,1.3-18.9-0.6-26.9-6.6c-7.4-5.6-11.6-13-12.7-22.2
+						c-1.3-10.9,1.9-20.7,8.5-29.3c7.1-9.3,16.5-15.2,28-17.3c9.4-1.7,18.4-0.6,26.5,4.9c5.3,3.5,9.1,8.3,11.6,14.1
+						C130,20.6,129.6,21.1,128.4,21.4z"/>
+				</g>
+				<g>
+					<path d="M161.5,76.7c-9.1-0.2-17.4-2.8-24.4-8.8c-5.9-5.1-9.6-11.6-10.8-19.3c-1.8-11.3,1.3-21.3,8.1-30.2
+						c7.3-9.6,16.1-14.6,28-16.7c10.2-1.8,19.8-0.8,28.5,5.1c7.9,5.4,12.8,12.7,14.1,22.3c1.7,13.5-2.2,24.5-11.5,33.9
+						c-6.6,6.7-14.7,10.9-24,12.8C166.8,76.3,164.1,76.4,161.5,76.7z M185.3,36.3c-0.1-1.3-0.1-2.3-0.3-3.3
+						c-1.8-9.9-10.9-15.5-20.4-13.3c-9.3,2.1-15.3,8-17.5,17.4c-1.8,7.8,2,15.7,9.2,18.9c5.5,2.4,11,2.1,16.3-0.6
+						C180.5,51.3,184.8,44.9,185.3,36.3z"/>
+				</g>
+			</g>
+		</g>
+	</g>
+</g>
+</svg>
+        <div class="language">Golang</div>
+        <p>I have mostly worked with it since joining Commercetools. While I wouldn't consider myself an expert, I love its simplicity.</p>
+    </div>
+    <div class="skill text-center">
+	<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 40 40" width="160px" height="160px">
+	<rect x="10" y="0" width="20" height="40" rx="10" ry="5" />
+	<rect x="0" y="10" width="40" height="20" rx="5" ry="10" />
+	<circle cx="14.5" cy="5" r="1.85" fill="white" />
+	<circle cx="25.5" cy="35" r="1.85" fill="white" />
+	<line x1="10" y1="9.5" x2="20" y2="9.5" stroke="white" />
+	<line x1="20" y1="30.5" x2="30" y2="30.5" stroke="white" />
+	<path d="m 9.5,30 c 0,-10 2.5,-10 10,-10 8.5,0 11,0 11,-10" stroke="white" fill="none" /></svg>
+        <div class="language">Python</div>
+        <p>This was my go-to language for scripts and desktop apps in the past, but I now use it only occasionally.</p>
+    </div>
+    <div class="skill text-center">
+      <svg width="160px" height="160px" viewBox="0 -10.44 722.846 722.846" xmlns="http://www.w3.org/2000/svg"><path d="M358.986 10.06a46.725 46.342 0 00-17.906 4.531L96.736 131.341a46.725 46.342 0 00-25.28 31.438l-60.282 262.25a46.725 46.342 0 006.344 35.531 46.725 46.342 0 002.656 3.688l169.125 210.28a46.725 46.342 0 0036.531 17.438l271.219-.062a46.725 46.342 0 0036.531-17.406l169.063-210.313a46.725 46.342 0 009.03-39.219L651.3 162.716a46.725 46.342 0 00-25.281-31.437L381.643 14.59a46.725 46.342 0 00-22.657-4.53z" fill="black"/><path d="M361.408 99.307c-8.077.001-14.626 7.276-14.625 16.25 0 .138.028.27.03.406-.011 1.22-.07 2.689-.03 3.75.192 5.176 1.32 9.138 2 13.907 1.23 10.206 2.26 18.667 1.625 26.531-.62 2.965-2.803 5.677-4.75 7.562l-.344 6.188a190.337 190.337 0 00-26.438 4.062c-37.974 8.623-70.67 28.184-95.562 54.594a245.167 245.167 0 01-5.281-3.75c-2.612.353-5.25 1.159-8.688-.844-6.545-4.405-12.506-10.486-19.719-17.812-3.305-3.504-5.698-6.841-9.625-10.219-.891-.767-2.252-1.804-3.25-2.594-3.07-2.447-6.69-3.724-10.187-3.843-4.496-.154-8.824 1.604-11.656 5.156-5.036 6.315-3.424 15.968 3.593 21.562.071.057.147.101.219.157.964.781 2.145 1.783 3.031 2.437 4.167 3.077 7.973 4.652 12.125 7.094 8.747 5.402 15.999 9.88 21.75 15.281 2.246 2.394 2.639 6.613 2.938 8.438l4.687 4.187c-25.093 37.764-36.707 84.41-29.843 131.938l-6.125 1.781c-1.615 2.085-3.896 5.365-6.282 6.344-7.525 2.37-15.994 3.24-26.218 4.312-4.8.4-8.943.161-14.032 1.125-1.12.212-2.68.619-3.906.906l-.125.032c-.067.015-.155.048-.219.062-8.62 2.083-14.157 10.006-12.375 17.813 1.783 7.808 10.203 12.556 18.875 10.687.063-.014.154-.017.219-.031.098-.022.184-.07.281-.094 1.21-.265 2.724-.56 3.782-.843 5.003-1.34 8.626-3.308 13.125-5.032 9.676-3.47 17.691-6.37 25.5-7.5 3.26-.255 6.697 2.012 8.406 2.969l6.375-1.094c14.67 45.483 45.414 82.245 84.344 105.313l-2.657 6.375c.958 2.475 2.014 5.824 1.3 8.27-2.838 7.36-7.7 15.13-13.237 23.792-2.681 4.002-5.425 7.108-7.844 11.688-.579 1.096-1.316 2.78-1.875 3.937-3.759 8.043-1.002 17.305 6.219 20.782 7.266 3.497 16.284-.192 20.187-8.25.006-.012.026-.02.031-.032.004-.009-.004-.022 0-.03.556-1.143 1.344-2.645 1.813-3.72 2.072-4.747 2.762-8.815 4.219-13.406 3.87-9.72 5.995-19.919 11.322-26.274 1.459-1.74 3.837-2.41 6.303-3.07l3.312-6c33.938 13.027 71.927 16.523 109.875 7.907a189.77 189.77 0 0025.094-7.563c.93 1.651 2.661 4.826 3.125 5.625 2.506.815 5.24 1.236 7.469 4.531 3.985 6.81 6.71 14.865 10.031 24.594 1.457 4.591 2.178 8.66 4.25 13.406.472 1.082 1.256 2.605 1.812 3.75 3.895 8.085 12.943 11.787 20.22 8.282 7.219-3.478 9.979-12.74 6.218-20.782-.559-1.158-1.327-2.841-1.906-3.937-2.42-4.58-5.163-7.655-7.844-11.656-5.537-8.662-10.13-15.858-12.969-23.22-1.187-3.796.2-6.157 1.125-8.624-.554-.635-1.739-4.22-2.437-5.906 40.457-23.889 70.298-62.022 84.312-106.063 1.893.298 5.182.88 6.25 1.094 2.2-1.45 4.222-3.344 8.188-3.031 7.808 1.129 15.823 4.03 25.5 7.5 4.498 1.723 8.121 3.723 13.125 5.062 1.057.283 2.572.547 3.781.813.097.023.183.071.281.093.066.015.156.017.219.032 8.672 1.866 17.094-2.88 18.875-10.688 1.78-7.807-3.754-15.732-12.375-17.812-1.254-.286-3.032-.77-4.25-1-5.09-.964-9.231-.727-14.031-1.125-10.225-1.072-18.694-1.943-26.219-4.313-3.068-1.19-5.251-4.841-6.313-6.344l-5.906-1.718c3.062-22.155 2.237-45.212-3.062-68.282-5.349-23.284-14.8-44.58-27.407-63.343 1.515-1.378 4.377-3.911 5.188-4.657.237-2.624.033-5.375 2.75-8.281 5.751-5.4 13.003-9.879 21.75-15.281 4.152-2.443 7.99-4.017 12.156-7.094.942-.696 2.23-1.798 3.219-2.594 7.015-5.596 8.63-15.248 3.594-21.562-5.037-6.314-14.797-6.91-21.813-1.313-.998.791-2.353 1.823-3.25 2.594-3.926 3.378-6.351 6.714-9.656 10.219-7.213 7.326-13.174 13.438-19.719 17.844-2.836 1.65-6.99 1.08-8.875.968l-5.562 3.969c-31.72-33.26-74.905-54.525-121.406-58.656-.13-1.949-.3-5.471-.344-6.532-1.904-1.821-4.204-3.376-4.781-7.312-.637-7.864.426-16.325 1.656-26.531.679-4.769 1.807-8.73 2-13.907.044-1.176-.027-2.884-.031-4.156-.001-8.974-6.548-16.25-14.625-16.25zm-18.313 113.438l-4.344 76.718-.312.157c-.292 6.863-5.94 12.343-12.875 12.343-2.841 0-5.463-.912-7.594-2.468l-.125.062-62.906-44.594c19.333-19.01 44.063-33.06 72.562-39.53a154.125 154.125 0 0115.594-2.688zm36.656 0c33.274 4.092 64.045 19.159 87.625 42.25l-62.5 44.312-.218-.093c-5.548 4.051-13.364 3.046-17.688-2.375a12.807 12.807 0 01-2.812-7.47l-.063-.03zM232.126 283.62l57.438 51.375-.063.312c5.185 4.507 5.95 12.328 1.625 17.75a12.892 12.892 0 01-6.687 4.406l-.063.25-73.625 21.25c-3.747-34.265 4.329-67.573 21.375-95.343zm258.157.03c8.534 13.833 14.996 29.283 18.843 46.032 3.801 16.548 4.755 33.067 3.188 49.031l-74-21.312-.063-.313c-6.626-1.81-10.699-8.551-9.156-15.312a12.786 12.786 0 014.094-6.844l-.031-.156 57.125-51.125zm-140.657 55.313h23.532l14.625 18.282-5.25 22.812-21.125 10.156-21.188-10.187-5.25-22.813zm75.438 62.563c1-.05 1.995.04 2.969.219l.125-.157 76.156 12.875c-11.146 31.314-32.473 58.44-60.969 76.594l-29.562-71.406.093-.125c-2.715-6.31.002-13.71 6.25-16.719 1.6-.77 3.271-1.197 4.938-1.281zm-127.906.312c5.811.082 11.024 4.116 12.375 10.032.632 2.77.324 5.513-.72 7.937l.22.281-29.25 70.688c-27.348-17.549-49.13-43.824-60.782-76.063l75.5-12.812.125.156c.845-.155 1.701-.23 2.532-.219zm63.78 30.97a12.764 12.764 0 016.032 1.28c2.56 1.233 4.537 3.174 5.781 5.5h.282l37.218 67.25a154.256 154.256 0 01-14.875 4.157c-28.464 6.463-56.838 4.504-82.53-4.25l37.124-67.125h.063a12.91 12.91 0 0110.906-6.813z" style="text-indent:0;text-align:start;line-height:normal;text-transform:none;block-progression:tb;marker:none;-inkscape-font-specification:Sans" font-weight="400" color="#000000" fill="#ffffff" stroke="#ffffff" stroke-width=".25" overflow="visible" font-family="Sans"/></svg>
+	<div class="language">Kubernetes</div>
+	<p>Working with it every day since I started at Cloudmobility.</p>
 </div>
