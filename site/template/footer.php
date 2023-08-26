d72a4251634a3a1fdf7e8ba4a8cd6ec29fcc178f
Bruno Jesus <bruno.fl.jesus@gmail.com>
2023-08-26 21:05:39 +0100

    Add GitHub link to footer

--- site/template/footer.php
+++ site/template/footer.php
@@ -3,6 +3,7 @@
 			<nav class="navigation">
 				<ul>
 					<li><a href="https://www.linkedin.com/in/bruno-jesus-0ab943a9/"><i class="fa fa-linkedin-square" aria-hidden="true"></i> LinkedIn</a></li>
+					<li><a href="https://github.com/brunofjesus"><i class="fa fa-github" aria-hidden="true"></i> GitHub</a></li>
 					<li><a href="https://gitlab.com/strang3quark"><i class="fa fa-gitlab" aria-hidden="true"></i> GitLab</a></li>
 				</ul>
 			</nav>			
